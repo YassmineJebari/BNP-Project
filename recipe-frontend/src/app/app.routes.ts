f4f57f4f89586e34202a037b6ed8a76e813fb994
sarrabenouhiba <sarrabenwahiba@gmail.com>
2025-10-30 10:22:51 +0100

    crud recette front

--- recipe-frontend/src/app/app.routes.ts
+++ recipe-frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { List } from './recipes/list/list';
+import { Add } from './recipes/add/add';
+import { Edit } from './recipes/edit/edit';
+import { Detail } from './recipes/detail/detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
+  { path: 'recipes', component: List },
+  { path: 'recipes/add', component: Add },
+  { path: 'recipes/edit/:id', component: Edit },
+  { path: 'recipes/detail/:id', component: Detail },
+  { path: '**', redirectTo: 'recipes' }
+];
