ed2d4501cda3376845aabb1ce130752d57711e53
YassmineJebari <yassminejbari03@gmail.com>
2025-11-06 21:51:18 +0100

    ajout backend gestion recettes

--- recipe-backend/recipe_backend/src/main/java/com/recipes/recipe_backend/config/JwtAuthenticationFilter.java
+++ recipe-backend/recipe_backend/src/main/java/com/recipes/recipe_backend/config/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.recipes.recipe_backend.config;
 
+import com.recipes.recipe_backend.gestion_utilisateur.service.JwtService; 
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -47,7 +49,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
 
-            if (jwtService.isTokenValid(jwt, userDetails)) {
+            if (jwtService.validateToken(jwt, userDetails)) { 
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
                         null,
