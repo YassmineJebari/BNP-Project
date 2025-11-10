652387f8ee6d49670d5e92a68032bfc7ba6ecfaa
sarrabenouhiba <sarrabenwahiba@gmail.com>
2025-11-10 21:11:39 +0100

    Configuration routes avec recipe-layout

--- recipe-frontend/src/app/features/recipes/recipes.routes.ts
+++ recipe-frontend/src/app/features/recipes/recipes.routes.ts
@@ -1,24 +1,31 @@
 import { Routes } from '@angular/router';
+import { RecipeLayout } from './components/recipe-layout/recipe-layout';
 
 export const RECIPES_ROUTES: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/list/list')
-      .then(m => m.List)
-  },
-  {
-    path: 'add',
-    loadComponent: () => import('./pages/add/add')
-      .then(m => m.Add)
-  },
-  {
-    path: 'detail/:id',
-    loadComponent: () => import('./pages/detail/detail')
-      .then(m => m.Detail)
-  },
-  {
-    path: 'edit/:id',
-    loadComponent: () => import('./pages/edit/edit')
-      .then(m => m.Edit)
+    component: RecipeLayout,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/list/list')
+          .then(m => m.List)
+      },
+      {
+        path: 'add',
+        loadComponent: () => import('./pages/add/add')
+          .then(m => m.Add)
+      },
+      {
+        path: 'detail/:id',
+        loadComponent: () => import('./pages/detail/detail')
+          .then(m => m.Detail)
+      },
+      {
+        path: 'edit/:id',
+        loadComponent: () => import('./pages/edit/edit')
+          .then(m => m.Edit)
+      }
+    ]
   }
 ];
